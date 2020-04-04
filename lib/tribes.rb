cba7a490d40c5df7931009368b721f8606c38d83
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-04-04 13:15:43 +0200

    Add method to calculate hash for mobile api

--- lib/tribes.rb
+++ lib/tribes.rb
@@ -7,8 +7,17 @@ require 'uri'
 require 'faraday'
 require 'faraday_middleware'
 require 'active_support/core_ext/hash'
+require 'digest'
+require 'json'
 
 module Tribes
   class Error < StandardError; end
   # Your code goes here...
+  def self.calculate_mobile_hash(data)
+    raise ArgumentError, 'Method accepts only arrays' unless data.is_a?(Array)
+
+    secret = '2sB2jaeNEG6C01QOTldcgCKO'
+    payload = secret + '-' + data.to_json
+    Digest::SHA1.hexdigest(payload)
+  end
 end
--- spec/tribes_spec.rb
+++ spec/tribes_spec.rb
@@ -10,4 +10,32 @@ RSpec.describe Tribes do
 
     expect { Net::HTTP.get(uri) }.to raise_error(WebMock::NetConnectNotAllowedError)
   end
+
+  describe '.calculate_mobile_hash' do
+    subject { Tribes.calculate_mobile_hash(data) }
+
+    context 'for login data array' do
+      let(:data) { ['korenchkin', 'rickenbacker1', '2.30.0'] }
+
+      it { expect(subject).to eq 'bf774c157a1724b38f8c01d2c7d12e8081821ecc' }
+    end
+
+    context 'for array with one element' do
+      let(:data) { ["5682439b4ec15f78a9b2d4d409bfb37e7d840a82"] }
+
+      it { expect(subject).to eq '8303726eafff094edec319dffdf1774973aa0880' }
+    end
+
+    context 'for single element' do
+      let(:data) { "5682439b4ec15f78a9b2d4d409bfb37e7d840a82" }
+
+      it { expect { subject }.to raise_error(ArgumentError, 'Method accepts only arrays') }
+    end
+
+    context 'for hash' do
+      let(:data) { { prop1: 'korenchkin', prop2: 'rickenbacker1', prop3: '2.30.0' } }
+
+      it { expect { subject }.to raise_error(ArgumentError, 'Method accepts only arrays') }
+    end
+  end
 end
