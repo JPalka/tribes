61b5f581128c834fbe33c76eff98cacf29b4282f
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-04-05 12:53:03 +0200

    Set default headers

--- lib/tribes/headers.rb
+++ lib/tribes/headers.rb
@@ -1,7 +1,20 @@
 module Tribes
   class Headers
     def initialize
-      @headers = {}
+      @headers = {
+        'Accept-Encoding' => 'deflate, gzip',
+        'User-Agent' => 'Mozilla/5.0 (Android; U; pl-PL) AppleWebKit/533.19.4 (KHTML,like Gecko) AdobeAIR/31.0',
+        'x-flash-version' => '31,0,0,101',
+        'Connection' => 'Keep-Alive',
+        'Cache-Control' => 'no-cache',
+        'Referer' => 'app:/staemme.swf',
+        'Content-Type' => 'flv-application/octet-stream; charset=UTF-8',
+        'x-ig-os-name' => 'android',
+        'x-ig-os-version' => '5.0',
+        'x-ig-model' => 'HTC One',
+        'x-ig-manufacturer' => 'HTC',
+        'IGMobileDevice' => 'Android'
+      }
     end
 
     def set(header, value)
--- spec/tribes/headers_spec.rb
+++ spec/tribes/headers_spec.rb
@@ -1,10 +1,32 @@
 
 RSpec.describe Tribes::Headers do
   let(:headers) { described_class.new }
-  let(:header) { 'Accept-Encoding' }
-  let(:value) { 'deflate, gzip' }
-  let(:header2) { 'Connection' }
-  let(:value2) { 'Keep-Alive' }
+  let(:header) { 'test header' }
+  let(:value) { 'test value' }
+  let(:header2) { 'tst header2' }
+  let(:value2) { 'test value2' }
+
+  describe '#initialize' do
+    it 'has default headers set' do
+      expect(subject.to_h).to eq(
+        {
+          'Accept-Encoding' => 'deflate, gzip',
+          'User-Agent' => 'Mozilla/5.0 (Android; U; pl-PL) AppleWebKit/533.19.4 (KHTML,like Gecko) AdobeAIR/31.0',
+          'x-flash-version' => '31,0,0,101',
+          'Connection' => 'Keep-Alive',
+          'Cache-Control' => 'no-cache',
+          'Referer' => 'app:/staemme.swf',
+          'Content-Type' => 'flv-application/octet-stream; charset=UTF-8',
+          'x-ig-os-name' => 'android',
+          'x-ig-os-version' => '5.0',
+          'x-ig-model' => 'HTC One',
+          'x-ig-manufacturer' => 'HTC',
+          'IGMobileDevice' => 'Android'
+        }
+      )
+    end
+  end
+
   describe '#set' do
     subject do
       headers.set(header, value)
