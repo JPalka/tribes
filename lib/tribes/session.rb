b263bb027b37f0f66123dee5986d37982fa0a419
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-05-13 20:41:17 +0200

    Add Session class

---
+++ lib/tribes/session.rb
@@ -0,0 +1,13 @@
+module Tribes
+  class Session
+    def initialize
+      @market_key = 'zz'
+      @username = ''
+      @password = ''
+      @token = ''
+      @player_id = ''
+      @login_url = ''
+      @session_id = ''
+    end
+  end
+end
