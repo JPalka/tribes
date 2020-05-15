051e0edb6efebf08fe8300715317a9d67fff9cb9
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-05-15 13:49:19 +0200

    Add DataList class. Refactor WorldList to use it

---
+++ lib/tribes/data_list.rb
@@ -0,0 +1,31 @@
+module Tribes
+  class DataList
+    attr_accessor :selected_element
+    def initialize(configuration)
+      @configuration = configuration
+      @list = []
+      @selected_element = nil
+    end
+
+    def download(session)
+      response = @controller.load(post_data(session))
+      @controller.check_errors(response)
+      load(response)
+      response
+    end
+
+    def load(_json_data)
+      throw 'load method not implemented'
+    end
+
+    def select(_entity_id)
+      throw 'select method not implemented'
+    end
+
+    private
+
+    def post_data(_session)
+      throw 'post_data method not implemented'
+    end
+  end
+end
--- lib/tribes/world_list.rb
+++ lib/tribes/world_list.rb
@@ -1,39 +1,34 @@
 module Tribes
-  class WorldList
+  class WorldList < DataList
     def initialize(configuration)
-      @worlds = []
-      @active_worlds = [] # worlds that you are already playing on
-
-      @selected_world = nil
-      @configuration = configuration
+      super(configuration)
+      @controller = ControllerServer.new(ServiceContainer::GET_WORLDS, @configuration)
     end
 
-    def download_worlds(session)
-      controller = ControllerServer.new(ServiceContainer::GET_WORLDS, @configuration)
-      json_response = controller.load([session.token])
-      controller.check_errors(json_response)
-      load_worlds(json_response['result'])
-      json_response
-    end
-
-    def load_worlds(json_data)
+    def load(json_data)
       json_data.each do |world_group, _val|
         next if world_group == 'current'
 
         json_data[world_group].each do |world|
-          @worlds.push(world)
+          @list.push(world)
         end
       end
     end
 
-    def change_world(world_id)
+    def select(world_id)
       # yeet if world is already selected
-      return false if world_id == @selected_world.to_h['server_name']
+      return false if world_id == @selected_element.to_h['server_name']
 
-      found_world = @worlds.find { |world| world['server_name'] == world_id }
+      found_world = @list.find { |world| world['server_name'] == world_id }
       return false unless found_world
 
-      @selected_world = found_world
+      @selected_element = found_world
+    end
+
+    private
+
+    def post_data(session)
+      [session.token]
     end
   end
 end
