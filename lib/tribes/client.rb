032e4700406c613075f4fc9af4d532cf1a87e5be
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-05-18 11:39:58 +0200

    Remove unused code

--- lib/tribes/client.rb
+++ lib/tribes/client.rb
@@ -3,7 +3,6 @@
 module Tribes
   class Client
     include Tribes::Parser
-    attr_reader :active_worlds
 
     def initialize(options = {})
       @configuration = Configuration.new
@@ -45,30 +44,24 @@ module Tribes
 
     def village_data
       controller = ControllerServer.new(ServiceContainer::GET_VILLAGE_DATA, @configuration)
-      json_response = controller.load([@session.session_id, @village_list.selected_element[0]])
-      json_response
+      controller.load([@session.session_id, @village_list.selected_element[0]])
     end
 
     def village_visual
       controller = ControllerServer.new(ServiceContainer::GET_VILLAGE_VISUAL, @configuration)
-      json_response = controller.load([@session.session_id, @village_list.selected_element[0]])
-      json_response
+      controller.load([@session.session_id, @village_list.selected_element[0]])
     end
 
     def player_info
       controller = ControllerServer.new(ServiceContainer::GET_PLAYER_INFO, @configuration)
-      json_response = controller.load([@session.session_id, @session.player_id])
-      json_response
+      controller.load([@session.session_id, @session.player_id])
     end
 
     def prod_building(building_id)
       throw 'wrong building type.' unless %w[wood iron clay].include?(building_id)
 
       controller = ControllerServer.new(ServiceContainer::GET_PROD_BUILDING, @configuration)
-      json_response = controller.load([@session.session_id,
-                                       @village_list.selected_element[0],
-                                       building_id.to_s])
-      json_response
+      controller.load([@session.session_id, @village_list.selected_element[0], building_id.to_s])
     end
 
     def change_world(world_id)
@@ -79,46 +72,6 @@ module Tribes
       else
         false
       end
-    end
-
-    private
-
-    def find_world_url(world_id)
-      result = ''
-      @active_worlds.each do |world|
-        result = world['url'] if world['server_name'] == world_id
-      end
-      result
-    end
-
-    def download_player_list
-      response = @connection.get('/map/player.txt')
-      parse_player_list(response.body)
-    end
-
-    def download_village_list
-      response = @connection.get('/map/village.txt')
-      parse_village_list(response.body)
-    end
-
-    def download_tribe_list
-      response = @connection.get('/map/ally.txt')
-      parse_tribe_list(response.body)
-    end
-
-    def download_world_config
-      response = @connection.get('/interface.php?func=get_config')
-      parse_config(response.body)
-    end
-
-    def download_building_info
-      response = @connection.get('/interface.php?func=get_building_info')
-      parse_config(response.body)
-    end
-
-    def download_unit_info
-      response = @connection.get('/interface.php?func=get_unit_info')
-      parse_config(response.body)
     end
   end
 end
