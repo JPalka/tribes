b11d75f0815f41f957426bfa2dab2134923b4fb7
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-05-29 22:55:31 +0200

    Add upgrading flags

--- lib/tribes/sites/flags_index.rb
+++ lib/tribes/sites/flags_index.rb
@@ -17,6 +17,16 @@ module Tribes
       def set_extractors
         [Tribes::Extractors::Flags.new]
       end
+
+      def upgrade_flag(category, level)
+        category_number = Tribes::FLAG_CATEGORIES.key(category) || category
+        begin
+          @browser.find("div#flag_box_#{category_number}_#{level} div.flag_upgrade").click
+          true
+        rescue Capybara::ElementNotFound
+          false
+        end
+      end
     end
   end
 end
