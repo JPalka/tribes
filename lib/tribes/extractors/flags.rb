f37869a3ea18ebcd095afe01616a6a5260dabdba
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-05-30 13:22:29 +0200

    Add extracting current flag

--- lib/tribes/extractors/flags.rb
+++ lib/tribes/extractors/flags.rb
@@ -14,10 +14,19 @@ module Tribes
               extract_flag_info(flag)
             end
         end
+        result[:active_flag] = flag_from_img(doc.css('div#current_flag img').attr('src'))
         result
       end
 
       private
+
+      # behold! extracting flag flag level and category from icon src because
+      # its nowhere to be found. Possible PITA.
+      # https://dsen.innogamescdn.com/asset/3cc5e90/graphic/flags/medium/1_5.png
+      def flag_from_img(img_src)
+        _crap, category, level = /([1-9])_([1-9])/.match(img_src).to_a
+        { Tribes::FLAG_CATEGORIES[category.to_i] => level }
+      end
 
       def extract_flag_info(div)
         div.css('.flag_count').text.to_i
