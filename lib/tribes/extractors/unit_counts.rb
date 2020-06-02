bac7952ce4b9f1e3c98d528768f95390c339c26e
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-06-02 22:47:42 +0200

    Add extracting unit info from village overview

--- lib/tribes/extractors/unit_counts.rb
+++ lib/tribes/extractors/unit_counts.rb
@@ -8,9 +8,30 @@ module Tribes
         result = { units: {} }
         result[:units][:available] =
           doc.css('a.units-entry-all').each_with_object({}) do |link, hash|
-            hash[link.attr('data-unit')] = link.content.tr('()', '')
+            hash[link.attr('data-unit')] = link.content.tr('()', '').to_i
           end
+        extract_village_overview(data, result)
         result
+      end
+
+      private
+
+      def extract_village_overview(data, result)
+        own = /(VillageOverview.units\[1\] = )({[^;]*)/.match(data)
+        supports = /(VillageOverview.units\[2\] = )({[^;]*)/.match(data)
+
+        temp = map_unit_count(parse_json_var(own))
+        temp_sup = map_unit_count(parse_json_var(supports))
+        result[:units][:available] = temp if temp
+        result[:units][:supports] = temp_sup if temp_sup
+      end
+
+      def map_unit_count(arr)
+        arr.map { |key, val| [key, val['count'].to_i] }.to_h if arr
+      end
+
+      def parse_json_var(var)
+        JSON.parse(var[2].to_s).to_h if var
       end
     end
   end
