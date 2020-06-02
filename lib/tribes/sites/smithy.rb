6d4db8b9dc7f8afc0bc488938dcc4e4f5f2172bb
Anatoly Korenchkin <anatoly.korenchkin@interia.pl>
2020-06-02 13:04:34 +0200

    Add researching unit types

--- lib/tribes/sites/smithy.rb
+++ lib/tribes/sites/smithy.rb
@@ -16,6 +16,19 @@ module Tribes
       def set_extractors
         [Tribes::Extractors::Tech.new]
       end
+
+      def research(unit)
+        research_buttons = @browser.has_css?('button.btn') ? @browser.all('button.btn') : []
+        unit_button = research_buttons.find do |button|
+          button_unit = /'.*'/.match(button[:onclick]).to_s.gsub("'", '')
+          unit == button_unit
+        end
+        if unit_button
+          unit_button.click
+          return true
+        end
+        false
+      end
     end
   end
 end
